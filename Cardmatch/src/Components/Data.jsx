2146c443730281b66f34531a06ecc29d7b6ff2de
QzyKuma <Quezjn24@gmail.com>
2024-07-02 10:41:29 -0400

    Adding code for Data component

--- Cardmatch/src/Components/Data.jsx
+++ Cardmatch/src/Components/Data.jsx
@@ -2,37 +2,77 @@
 
 const Data = [
     {
-
+        id: 1,
+        name: "react",
+        img:
+            "",
+        matched: false,
     },
     {
-
+        id: 2,
+        name: "JavaScript",
+        img:"",
+        matched: false,
     },
     {
-
+        id: 3,
+        name: "CSS",
+        img:"",
+        matched: false,
     },
     {
-
+        id: 4,
+        name: "Node",
+        img:"",
+        matched: false,
     },
     {
-
+        id: 5,
+        name: "HTML",
+        img:"",
+        matched: false,
     },
     {
-
+        id: 6,
+        name: "Python",
+        img:"",
+        matched: false,
     },
     {
-
+        id: 7,
+        name: "React",
+        img:"",
+        matched: false,
     },
     {
-
+        id: 8,
+        name: "JavaScript",
+        img:"",
+        matched: false,
     },
     {
-
+        id: 9,
+        name: "CSS",
+        img:"",
+        matched: false,
     },
     {
-
+        id: 10,
+        name: "Node",
+        img:"",
+        matched: false,
     },
     {
-
+        id: 11,
+        name: "HTML",
+        img:"",
+        matched: false,
+    },
+    {
+        id: 12,
+        name: "Python",
+        img:"",
+        matched: false,
     },
 
 ];
